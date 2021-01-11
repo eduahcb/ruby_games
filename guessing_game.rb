80b05c26f5ab25eb8687d99b4a3e25fdd83625b5
Eduardo Barbosa <dev_eduardo@hotmail.com>
2021-01-11 09:52:42 -0300

    refactoring code.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,7 +1,7 @@
 def welcome
   puts 'Bem-vindo ao jogo de adivinhação'
   puts 'Qual é o seu nome?'
-  player = gets
+  player = gets.strip
 
   puts "\n\n"
   puts "O Jogo irá iniciar, #{player}"
@@ -15,32 +15,46 @@ end
 def select_a_number
   puts "\n\n"
   puts 'Escolha um numero:'
-  gets
+  gets.strip
 end
 
-def result_game; end
-
-welcome
-secret_number = choosing_a_random_number
-puts "\n"
-
-limit_of_tries = 3
-(1..limit_of_tries).each do |tries|
+def asking_for_a_number(kicks, limit_of_tries, tries)
   puts "Tentativa #{tries} de #{limit_of_tries}"
+  puts "Chutes até agora #{kicks}"
   kick = select_a_number
   puts "Será que acertou? Você chutou #{kick}"
+  kick.to_i
+end
 
-  that_is_right = secret_number == kick.to_i
+def check_if_is_right(secret_number, kick)
+  that_is_right = secret_number == kick
   if that_is_right
     puts 'acertou!!!'
-    break
-  else
-    is_greater = secret_number > kick.to_i
+    return true
+  end
+  is_greater = secret_number > kick
 
-    if is_greater
-      puts 'O numero é maior!'
-    else
-      puts 'O numero é menor!'
-    end
+  if is_greater
+    puts 'O numero é maior!'
+  else
+    puts 'O numero é menor!'
+  end
+  false
+end
+
+def main
+  welcome
+  secret_number = choosing_a_random_number
+
+  kicks = []
+  limit_of_tries = 3
+
+  (1..limit_of_tries).each do |tries|
+    kick = asking_for_a_number kicks, limit_of_tries, tries
+    kicks << kick
+
+    break if check_if_is_right secret_number, kick
   end
 end
+
+main
